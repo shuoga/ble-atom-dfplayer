bde034247c75e9758a0f1fff118991ee46b9f659
shuoga <shuichiro_ogawa_1983@yahoo.co.jp>
2024-04-05 09:17:10 +0900

    change atom to atom lite

--- ble_atom_ receiver/ble_atom_ receiver.ino
+++ ble_atom_ receiver/ble_atom_ receiver.ino
@@ -1,74 +1,59 @@
 #include <M5Atom.h>
 #include <BLEDevice.h>
-
 #include <DFPlayer.h>
 #include <SoftwareSerial.h>
 
-uint8_t seq;                     // remember number of boots in RTC Memory
-#define MyManufacturerId 0xffee  // test manufacturer ID
-
+#define MyManufacturerId 0xddee  // テスト用の製造者ID
 #define MP3_RX_PIN 25
 #define MP3_TX_PIN 21
 #define MP3_SERIAL_SPEED 9600
 
 BLEScan* pBLEScan;
-
 SoftwareSerial mySerial(MP3_RX_PIN, MP3_TX_PIN);
 DFPlayer mp3;
+
+bool scanStarted = false;  // スキャンが開始されたかどうかのフラグ
+
+// BLEデバイスが見つかった時のコールバックを処理するクラス
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice advertisedDevice) override {
+    if (advertisedDevice.haveManufacturerData()) {
+      std::string manData = advertisedDevice.getManufacturerData();
+      uint16_t foundManuId = (manData[1] << 8) | manData[0];
+      if (foundManuId == MyManufacturerId) {
+        // Serial.println("Desired device found, stopping scan...");
+        pBLEScan->stop();            // スキャンを停止
+        scanStarted = false;         // スキャンのフラグをリセット
+        uint8_t value = manData[2];  // 最後のバイトをボリューム値として使用
+        if (value > 30) value = 30;  // ボリュームの最大値を30に制限
+        mp3.setVolume(value);        // ボリュームを設定
+        Serial.printf("New volume set to: %d\n", value);
+      }
+    }
+  }
+};
 
 void setup() {
   M5.begin();
   Serial.begin(115200);
-  Serial.print("\r\nscan start.\r\n");
-
   mySerial.begin(MP3_SERIAL_SPEED);
   mp3.begin(mySerial);
-  mp3.setVolume(20);
   delay(1000);
-
-  BLEDevice::init("");
-  pBLEScan = BLEDevice::getScan();
-  pBLEScan->setActiveScan(false);
+  mp3.setVolume(0);      // 初期音量の設定
+  mp3.playMP3Folder(1);  // MP3フォルダの特定のトラックを再生
+  mp3.repeatCurrentTrack(true);
+  Serial.println("scan start.");
+  BLEDevice::init("ATOM-LITE-RECIEVER");
+  pBLEScan = BLEDevice::getScan();  // BLEスキャンを設定
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true);  // アクティブスキャンを使用
 }
 
 void loop() {
-  //bool found = false;
-  float temp, humid, press;
-
-  BLEScanResults foundDevices = pBLEScan->start(3);
-  int count = foundDevices.getCount();
-  Serial.printf("device count: %d\r\n", count);
-  for (int i = 0; i < count; i++) {
-    BLEAdvertisedDevice d = foundDevices.getDevice(i);
-    // data check
-    // Serial.print("Device Address: ");
-    // Serial.println(d.getAddress().toString().c_str());
-    // Serial.print("Device Name: ");
-    // if (d.haveName()) {
-    //   Serial.println(d.getName().c_str());
-    // } else {
-    //   Serial.println("");
-    // }
-    // Serial.print("Device Manufacturer Data: ");
-    // if (d.haveManufacturerData()) {
-    //   Serial.println(d.getAddress().toString().c_str());
-    // }
-    // end
-    if (d.haveManufacturerData()) {
-      std::string data = d.getManufacturerData();
-      int manu = data[1] << 8 | data[0];
-      Serial.printf("manu: %d\r\n", manu);
-      Serial.printf("myid: %d\r\n", MyManufacturerId);
-      if (manu == MyManufacturerId && seq != data[2]) {
-        Serial.printf("data[0]: %d, data[1]: %d, data[2]: %d, data[3]: %d, data[4]: %d, data[5]: %d, data[6]: %d, data[7]: %d, data[8]: %d\r\n", data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8]);
-        //found = true;
-        seq = data[2];
-        temp = (float)(data[4] << 8 | data[3]) / 100.0;
-        humid = (float)(data[6] << 8 | data[5]) / 100.0;
-        press = (float)(data[8] << 8 | data[7]) * 10.0 / 100.0;
-        Serial.printf(">>> seq: %d, t: %.1f, h: %.1f, p: %.1f\r\n", seq, temp, humid, press);
-        mp3.playTrack(1);
-      }
-    }
+  if (!scanStarted) {
+    pBLEScan->start(1, false);  // スキャン開始（指定された秒数でスキャンを開始）
+    scanStarted = true;
+  } else {
+    scanStarted = false;
   }
 }
